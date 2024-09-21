773fcd75366dfebb920c26bb32faf21f0855aa51
Sagar Dhadke <peaceunique2003@gmail.com>
2024-09-21 20:30:25 +0530

    Remove Unused code

---
+++ lib/User.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyUsers extends StatefulWidget {
+  const MyUsers({super.key});
+
+  @override
+  State<MyUsers> createState() => _MyUsersState();
+}
+
+class _MyUsersState extends State<MyUsers> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
