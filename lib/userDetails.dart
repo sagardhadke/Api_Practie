0defa95294d073f08db041a12153600b12c324b5
Sagar Dhadke <peaceunique2003@gmail.com>
2024-09-21 21:23:26 +0530

    Complete Details UI

--- lib/userDetails.dart
+++ lib/userDetails.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class MyUserDetails extends StatefulWidget {
   const MyUserDetails({super.key});
@@ -17,8 +18,134 @@ class _MyUserDetailsState extends State<MyUserDetails> {
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Center(
+                child: Container(
+                    height: 100,
+                    width: 100,
+                    decoration:
+                        BoxDecoration(borderRadius: BorderRadius.circular(125)),
+                    child: CircleAvatar(
+                        radius: 20,
+                        backgroundImage: NetworkImage(
+                            "https://cdn.dribbble.com/users/16986/screenshots/2200438/media/a3456ad146484d9240c3261451fc7262.png"))),
+              ),
+              Text(
+                "sagar Dhadke",
+                style: TextStyle(fontSize: 21, fontWeight: FontWeight.bold),
+              ),
+              Text("sagardhadke",
+                  style: GoogleFonts.roboto(
+                      textStyle: TextStyle(fontSize: 18, color: Colors.grey)))
+            ],
+          ),
+          SizedBox(height: 15),
+          Padding(
+            padding: const EdgeInsets.all(18.0),
+            child: Row(
+              children: [
+                Icon(Icons.phone),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Mobile",
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        "+91 98765431210",
+                        style: TextStyle(
+                          fontSize: 15,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 15),
+            child: Row(
+              children: [
+                Icon(Icons.email_outlined),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Email",
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        "peaceunique2003@gmail.com",
+                        style: TextStyle(
+                          fontSize: 15,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 18.0),
+            child: Row(
+              children: [
+                Icon(Icons.date_range),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Date of Birth",
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        "00-00-0000",
+                        style: TextStyle(
+                          fontSize: 15,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 15),
+          Padding(
+            padding: const EdgeInsets.all(18.0),
+            child: SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                  style:
+                      ElevatedButton.styleFrom(backgroundColor: Colors.amber),
+                  onPressed: () {
+                    //here is my API code
+                  },
+                  child: Text(
+                    "Fetch Data",
+                    style: GoogleFonts.roboto(
+                        textStyle: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                            fontSize: 15)),
+                  )),
+            ),
+          )
         ],
       ),
     );
